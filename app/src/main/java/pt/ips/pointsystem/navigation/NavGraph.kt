a5bee422a9a8910e35e1149905038243ce748d50
ImSynx <pedrofq2006@gmail.com>
2025-12-11 23:38:40 +0000

    ecrã de login

--- app/src/main/java/pt/ips/pointsystem/navigation/NavGraph.kt
+++ app/src/main/java/pt/ips/pointsystem/navigation/NavGraph.kt
@@ -1,5 +1,6 @@
 package pt.ips.pointsystem.navigation
 
+import PerfilScreen
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -11,7 +12,6 @@ import pt.ips.pointsystem.model.Screen
 import pt.ips.pointsystem.ui.HistoricoScreen
 import pt.ips.pointsystem.ui.HomeScreen
 import pt.ips.pointsystem.ui.LoginScreen
-import pt.ips.pointsystem.ui.PerfilScreen
 import pt.ips.pointsystem.ui.PontoScreen
 
 @Composable
