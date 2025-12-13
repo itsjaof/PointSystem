95acc3bb881dea0df433bf3487f205e4794e1358
João Fonseca <fjoao.quiterio@gmail.com>
2025-12-13 15:00:04 +0000

    fix: atualiza icones descontinuado + remove imports desnecessários

--- app/src/main/java/pt/ips/pointsystem/ui/HistoricoScreen.kt
+++ app/src/main/java/pt/ips/pointsystem/ui/HistoricoScreen.kt
@@ -1,25 +1,39 @@
 package pt.ips.pointsystem.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Login
+import androidx.compose.material.icons.automirrored.filled.Logout
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Coffee
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.History
-import androidx.compose.material.icons.filled.Login
-import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Pause
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
 
 
 data class Picagem(
@@ -117,10 +131,10 @@ fun PicagemItem(picagem: Picagem) {
 
     val (icon, backgroundColor, title) = when (picagem.tipo) {
         TipoPicagem.ENTRADA ->
-            Triple(Icons.Default.Login, Color(0xFFDFF5E1), "Entrada")
+            Triple(Icons.AutoMirrored.Filled.Login, Color(0xFFDFF5E1), "Entrada")
 
         TipoPicagem.SAIDA ->
-            Triple(Icons.Default.Logout, Color(0xFFE3E8FF), "Saída")
+            Triple(Icons.AutoMirrored.Filled.Logout, Color(0xFFE3E8FF), "Saída")
 
         TipoPicagem.PAUSA ->
             Triple(Icons.Default.Pause, Color(0xFFFFF2CC), "Pausa")
